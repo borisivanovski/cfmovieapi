33796c5057541b7f4c371a7db1c906ce6f5cd61c
borisivanovski <114216313+borisivanovski@users.noreply.github.com>
2022-12-16 11:12:16 +0100

    Update Mongoose
    define the models for MongoDB and write business logic for your API and database using Mongoose.
    Update:  index.js  , models.js  , package-lock.json , package.json

--- index.js
+++ index.js
@@ -1,192 +1,237 @@
-const { addListener } = require("nodemon");
 
 const express = require("express"),
-  app = express(),
-  bodyParser = require("body-parser"),
-  uuid = require("uuid");
+bodyParser = require("body-parser"),
+uuid = require("uuid");
 
-app.use(bodyParser.json());
+const morgan = require("morgan");
+const app = express();
+const mongoose = require("mongoose");
+const Models = require("./models.js");
 
-let users = [
-    {
-        id: 1,
-        name: "Dylan",
-        favoriteMovies: []
-    },
-    {
-        id: 2,
-        name: "Nate",
-        favoriteMovies: ["Harry Potter and the Chamber of Secrets"]
-    },
-]
+const movieSchema = require("./models.js");
 
-let movies = [
-  {
-    "Title": "Harry Potter and the Chamber of Secrets",
-    "Description":
-      "The Chamber of Secrets was home to an ancient Basilisk, which, according to legend, was intended to be used to purge the area of Muggle-born students. The Chamber is flanked with toweringpillars entwined with carved wood, and a tall statue of Salazar Slytherin is at the far end",
-    "Genre": {
-      "Name": "Fantasy",
-      "Description": "Fantasy films are films that belong to the fantasy genre with fantastic themes, usually magic, supernatural events, mythology, folklore, or exotic fantasy worlds. The genre is considered a form of speculative fiction alongside science fiction films and horror films, although the genres do overlap."
-    },
-    "Director": {
-      "Name": "Chris Columbus",
-      "Bio": "Born in Spangler, Pennsylvania, Columbus studied film at the Tisch School of the Arts where he developed an interest in filmmaking . After writing screenplays for several teen comedies in the mid-1980s, he made his directorial debut with a teen adventure, Adventures in Babysitting (1987).",
-      "Birth": 1958.0
-    },
-    "ImageURL": "https://www.google.com/search?q=Harry+Potter+and+the+Chamber+of+Secrets+movie+image&tbm=isch&ved=2ahUKEwiKoL2e7ur7AhUMlRoKHdc-DK0Q2-cCegQIABAA&oq=Harry+Potter+and+the+Chamber+of+Secrets+movie+image&gs_lcp=CgNpbWcQAzIFCAAQgAQ6BAgAEEM6BwgAEIAEEBhQ_whYlRVgtx5oAHAAeACAAZ0BiAHlBZIBAzQuM5gBAKABAaoBC2d3cy13aXotaW1nwAEB&sclient=img&ei=F0mSY8q4MIyqatf9sOgK&bih=577&biw=1280#imgrc=d9ikkQRgb365VM",
-    "Featured": false
-  },
-  {
-    "Title": "Lord of the Rings: The Fellowship of the Ring",
-    "Description":
-      "The film is about power and greed, innocence and enlightenment. Ultimately, it describes a battle of good against evil, of kindness and trust against suspicion, and of fellowship against the desire for individual power. ",
-    "Genre": {
-      "Name": "Horror",
-      "Description": "Horror films may incorporate incidents of physical violence and psychological terror; they may be studies of deformed, disturbed, psychotic, or evil characters; stories of terrifying monsters or malevolent animals; or mystery thrillers that use atmosphere to build suspense."
-    },
-    "Director": {
-      "Name": "Peter Jackson",
-      "Bio": "Born on October 31, 1961, in New Zeland, Peter Jackson started his prolific career as a child, creating short films with a 8-mm movie camera. Without any formal training, Jackson has directed a number of successful films ranging across all genres.",
-      "Birth": 1961.0
-    },
-    "ImageURL": "https://www.google.com/search?q=Lord+of+the+Rings%3A+The+Fellowship+of+the+Ring+movie+image&tbm=isch&ved=2ahUKEwi24cPi7ur7AhUCphoKHarlAEoQ2-cCegQIABAA&oq=Lord+of+the+Rings%3A+The+Fellowship+of+the+Ring+movie+image&gs_lcp=CgNpbWcQAzoFCAAQgARQ-A5Y-A5gihtoAHAAeACAAVGIAZcBkgEBMpgBAKABAaoBC2d3cy13aXotaW1nwAEB&sclient=img&ei=pkmSY7b2HoLMaqrLg9AE&bih=577&biw=1280",
-    "Featured": false
-  },
-  {
-    "Title": "Alice in Wonderland",
-    "Description":
-      "Alice's Adventures in Wonderland represents the child's struggle to survive in the confusing world of adults . To understand our adult world, Alice has to overcome the open-mindedness that is characteristic for children. Apparently, adults need rules to live by. ",
-    "Genre": {
-      "Name": "Drama",
-      "Description": "The drama genre features stories with high stakes and many conflicts . They're plot-driven and demand that every character and scene move the story forward. Dramas follow a clearly defined narrative plot structure, depicting real-life scenarios or extreme situations with emotionally-driven characters."
-    },
-    "Director": {
-      "Name": "Tim Burton",
-      "Bio": "Tim Burton was born Timothy Walter Burton on August 25, 1958, in Burbank, California. As a child, Burton was engrossed with the classic horror films of Roger Corman—many of which featured quintessential screen villain Vincent Price.",
-      "Birth": 1958.0
-    },
-      "ImageURL": "https://www.google.com/search?q=tim+burton+alice+in+wonderland+images&tbm=isch&ved=2ahUKEwj-yoPK8er7AhVThHMKHasECDUQ2-cCegQIABAA&oq=alice+in+wonderland+tim+burton+image&gs_lcp=CgNpbWcQARgAMgYIABAIEB46BQgAEIAEOgcIABCABBAYUMwOWKI9YMhCaABwAHgAgAGCAYgB7gaSAQQxMC4xmAEAoAEBqgELZ3dzLXdpei1pbWfAAQE&sclient=img&ei=mEySY_6eGtOIzgOriaCoAw&bih=577&biw=1280",
-      "Featured": false
-  },
-];
+const Movies = Models.Movie;
+const Users = Models.User;
+const Genres = Models.Genres;
+const Director = Models.Director;
 
-// CREATE
-app.post('/users', (req, res) => {
-    const newUser = req.body;
 
-    if (newUser.name) {
-        newUser.id = uuid.v4();
-        users.push(newUser);
-        res.status(201).json(newUser)
-    } else {
-        res.status(400).send('users need names')
-    }
-})
+let Movie = mongoose.model('Movie', movieSchema);
+let User = mongoose.model('User', userSchema);
 
-// UPDATE
-app.put('/users/:id', (req, res) => {
-    const { id } = req.params;
-    const updatedUser = req.body;
 
-    let user = users.find( user => user.id == id );
+// mongose.connect(process.env.Connection_URI, {
+  // useNewUrlParser: true,
+  // useUnifiedTopology: true;
+  // });
+mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
 
-    if (user) {
-        user.name = updatedUser.name;
-        res.status(200).json(user)
-    } else {
-        res.status(400).send('no such user')
-    }
-})
+//log requests to server
+app.use(morgan("common"));
 
-// CREATE
-app.post('/users/:id/:movieTitle', (req, res) => {
-    const { id, movieTitle } = req.params;
+//import auth into index
 
-    let user = users.find( user => user.id == id );
+//default text response when at /
+app.get("/", (reg, res) => {
+  res.send("Welcome ro MyFlix!")
+});
 
-    if (user) {
-        user.favoriteMovies.push(movieTitle);
-        res.status(200).send(`${movieTitle} has been added to user ${id}'s array`);;
-    } else {
-        res.status(400).send('no such user')
-    }
-})
-
-// DELETE
-app.delete('/users/:id/:movieTitle', (req, res) => {
-    const { id, movieTitle } = req.params;
-
-    let user = users.find( user => user.id == id );
-
-    if (user) {
-        user.favoriteMovies = user.favoriteMovies.filter( title => title !== movieTitle);
-        res.status(200).send(`${movieTitle} has been removed from user ${id}'s array`);;
-    } else {
-        res.status(400).send('no such user')
-    }
-})
-
-// DELETE
-app.delete('/users/:id', (req, res) => {
-    const { id } = req.params;
-
-    let user = users.find( user => user.id == id );
-
-    if (user) {
-        users = users.filter( user => user.id != id);
-        res.status(200).send(`user ${id} has been deleted`);
-    } else {
-        res.status(400).send('no such user')
-    }
-})
-
-// READ
+//return JSON object when at /movies
 app.get("/movies", (req, res) => {
-  res.status(200).json(movies);
+  Movies.find()
+  .then((movies) => {
+    res.status(201).json(movies);
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send("Error: " + err);
+  });
 });
 
-// READ
-app.get("/movies/:title", (req, res) => {
-  const { title } = req.params;
-  const movie = movies.find((movie) => movie.Title === title);
-
-  if (movie) {
-    res.status(200).json(movie);
-  } else {
-    res.status(400).send("no such movie");
-  }
+app.get("/users", function (req, res) {
+  Users.find()
+    .then(function (users) {
+      res.status(201).json(users);
+    })
+    .catch(function (err) {
+      console.error(err) ;
+      res.status(500).send("Error: " + err);
+    });
 });
 
-// READ
-app.get("/movies/genre/:genreName", (req, res) => {
-  const { genreName } = req.params;
-  const genre = movies.find((movie) => movie.Genre.Name === genreName).Genre;
 
-  if (genre) {
-    res.status(200).json(genre);
-  } else {
-    res.status(400).send("no such genre");
-  }
+// GET JSON movie info when looking for specific title
+app.get("/movies/:Title", (req, res) => {
+  Movies.findOne({ Title: req.params.Title })
+     .then((movie) => {
+      res.json(movie);
+     })
+     .catch((err) => {
+       console.error(err);
+       res.status(500).send("Error: " + err);
+     });
 });
 
-// READ
-app.get("/movies/directors/:directorName", (req, res) => {
-    const { directorName } = req.params;
-    const director = movies.find((movie) => movie.Director.Name === directorName).Director;
-  
-    if (director) {
-      res.status(200).json(director);
+//GET JSON genre info when looking for specific genre
+app.get("/genre/:Name", (req, res) => {
+  Genres.findOne({ Name: req.params.Name })
+     .then((genre) => {
+      res.json(genre.Description);
+     })
+     .catch((err) => {
+       console.error(err);
+       res.status(500).send("Error: " + err);
+     });
+});
+
+//get info on director when looking for specific director
+app.get("/director/:Name", (req, res) => {
+  Directors.findOne({ Name: req.params.Name })
+     .then((director) => {
+      res.json(director);
+     })
+     .catch((err) => {
+       console.error(err);
+       res.status(500).send("Error: " + err);
+     });
+});
+
+//allow users to register
+app.post('/users', (req, res) => {
+  Users.findOne({ Username: req.body.Username })
+    .then((user) => {
+      if (user) {
+        return res.status(400).send(req.body.Username + 'already exists');
+      } else {
+        Users.create({
+            Username: req.body.Username,
+            Password: req.body.Password,
+            Email: req.body.Email,
+            Birthday: req.body.Birthday
+          })
+          .then((user) =>{res.status(201).json(user) })
+        .catch((error) => {
+          console.error(error);
+          res.status(500).send('Error: ' + error);
+        })
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send('Error: ' + error);
+    });
+});
+
+
+// Get all users
+app.get('/users', (req, res) => {
+  Users.find()
+    .then((users) => {
+      res.status(201).json(users);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
+
+
+// Get a user by username
+app.get('/users/:Username', (req, res) => {
+  Users.findOne({ Username: req.params.Username })
+    .then((user) => {
+      res.json(user);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
+
+
+//allow users to update their user info, by username
+app.put("/users/:Username", (req, res) => {
+  Users.findOneAndUpdate({ Username: req.params.Username }, {
+        $set: {
+          Username: req.body.Username,
+          Password: req.body.Password,
+          Email: req.body.Email,
+          Birthday: req.body.Birthday,
+        },
+      }, 
+      { new: true }, // This line makes sure that the updated document is running
+      (err, updatedUser) => {
+        if (err) {
+          console.error(err);
+          res.status(500).send("Error: " + err);
+        } else {
+          res.json(updatedUser);
+        }
+      });
+   });
+
+// Delete a user by username
+app.delete('/users/:Username', (req, res) => {
+  Users.findOneAndRemove({ Username: req.params.Username })
+    .then((user) => {
+      if (!user) {
+        res.status(400).send(req.params.Username + ' was not found');
+      } else {
+        res.status(200).send(req.params.Username + ' was deleted.');
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
+
+// Add a movie to a user's list of favorites
+app.post('/users/:Username/movies/:MovieID', (req, res) => {
+  Users.findOneAndUpdate({ Username: req.params.Username }, {
+     $push: { FavoriteMovies: req.params.MovieID }
+   },
+   { new: true }, // This line makes sure that the updated document is returned
+  (err, updatedUser) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
     } else {
-      res.status(400).send("no such director");
+      res.json(updatedUser);
     }
   });
+});
+
+// remove a movie from username's list
+app.delete("/users/:Username/Movies/:MovieID", (req, res) => {
+  Users.findOneAndUpdate(
+    { Username: req.params.Username },
+    { $pull: {Fav: req.params.MovieID } },
+    { new: true },
+    (error, updatedUser) => {
+      if (error) {
+        console.error(error);
+        res.status(500).send ("Error: " + error);
+      } else {
+        res.json(updatedUser);
+      }
+    });
+});
+
+
+//access documentation.html using express.static
+app.use("/documentation", express.static("public"));
+
+//error handling
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Error");
+});
+
+//listen on port
+app.listen(8080, () => console.log("My app is listening on port 8080."))
 
 
 
 
 
-
-
-
-app.listen(8080, () => console.log("Listening on 8080."));
